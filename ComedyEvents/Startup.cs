1e9b5ddaba7ba4b85efae6609984350eb133dc95
Devashish <devashishdas508@gmail.com>
2020-07-30 19:03:53 +0530

    Changes in Startup page added

--- ComedyEvents/Startup.cs
+++ ComedyEvents/Startup.cs
@@ -1,28 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
-using ComedyEvents.Context;
+using  ComedyEvents.Data;
 using ComedyEvents.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using AutoMapper;
+using ComedyEvents.Context;
 
 namespace ComedyEvents
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<EventContext>();
+            //services.AddDbContext<EventContext>();
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
             services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<ICommonRepository, CommonRepository>();
             services.AddAutoMapper();
+            services.AddDbContext<EventContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ComedyEvent")));
+            services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddAuthentication(Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                                  .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    }; 
+                });
 
             var mappingConfig = new MapperConfiguration(mc =>
             {
